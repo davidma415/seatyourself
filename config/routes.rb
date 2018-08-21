af037daa7de1060d1a5e8375ec2578498c1b0281
“Tyler <tylerpalef@gmail.com>
2018-08-21 14:50:57 -0400

    Created routes for the user, restaurant, reservations.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+resources :user, only: [:new, :create, :edit, :update]
+resources :restaurants do
+  resources :reservations, only: [:index, :new, :create]
+end
+resources :reservations, only: [:show, :edit, :update, :destroy]
+resources :sessions, only: [:new, :create]
+
+root 'restaurant#index'
+
 end
