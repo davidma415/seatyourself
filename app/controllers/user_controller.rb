7d1495bfc55b9b384427e70e6ebd68a333768533
“Tyler <tylerpalef@gmail.com>
2018-08-21 14:53:56 -0400

    updates to the application, session and user controller. No changes to the restaurant or reservation controller.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,28 @@
 class UserController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(
+      username: params[:user][:username],
+      name: params[:user][:name],
+      email: params[:user][:email],
+      password: params[:user][:password],
+      password_confirmation: params[:user][:password_confirmation]
+    )
+
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      flash.now[:alert] = @user.errors.full_messages
+      render :new
+    end
+  end
+
+end
+
+
 end
