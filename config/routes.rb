48dafef4864847a38ecaee0d65e9bec406649eba
David Ma <davidma415@gmail.com>
2018-08-22 14:39:30 -0400

    Fixed routing differences

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 resource :user, only: [:new, :create, :edit, :update, :show]
 resources :restaurants do
-resources :reservations
+  resources :reservations, only: [:index, :new, :create]
 end
+resources :reservations, only: [:show, :edit, :update, :destroy]
 resources :sessions, only: [:new, :create, :destroy]
 
 root 'restaurants#index'
