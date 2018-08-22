c3ebe5da266875761b553387057340996015a897
David Ma <davidma415@gmail.com>
2018-08-22 10:54:52 -0400

    Added functions to edit and show user, as well as links to respective pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,24 +23,28 @@ class UsersController < ApplicationController
     end
   end
   #
-  # def edit
-  #   @user = User.find(params[:id])
-  # end
-  #
-  # def update
-  #   @user = User.find(params[:id])
-  #   @user.first_name = params[:user][:first_name]
-  #   @user.last_name = params[:user][:last_name]
-  #   @user.email = params[:user][:email]
-  #   @user.password_digest = params[:user][:password_digest]
-  #
-  #   if @user.save
-  #     redirect_to user_path
-  #   else
-  #     render :edit
-  #   end
-  #
-  #
-  # end
+  def edit
+    @user = User.find(session[:user_id])
+  end
+
+  def show
+    @user = User.find(session[:user_id])
+  end
+
+  def update
+    @user = User.find(session[:user_id])
+    @user.first_name = params[:user][:first_name]
+    @user.last_name = params[:user][:last_name]
+    @user.username = params[:user][:username]
+    @user.email = params[:user][:email]
+    @user.password = params[:user][:password]
+    @user.password_confirmation = params[:user][:password_confirmation]
+
+    if @user.save
+      redirect_to user_path
+    else
+      render :edit
+    end
+  end
 
 end
