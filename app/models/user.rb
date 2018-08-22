c6687c3308f14b73eeb3328b22ebf94c1a814a73
David Ma <davidma415@gmail.com>
2018-08-22 10:31:40 -0400

    Finished login and sessions features

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   validates :first_name, :last_name, :username, :email, :password, :password_confirmation, presence: true
+  validates :username, uniqueness: true
 
   has_secure_password
   has_many :reservations
