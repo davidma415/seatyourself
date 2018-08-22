def7f2f6ba7548261bd52895103de14126d5a227
David Ma <davidma415@gmail.com>
2018-08-22 02:04:25 -0400

    Added bcrypt to gemfile and added a page to create users along with validations - session will need to be configured for editing and viewing user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,24 +3,25 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  #
-  # def create
-  #   @user = User.new(
-  #     username: params[:user][:username],
-  #     name: params[:user][:name],
-  #     email: params[:user][:email],
-  #     password: params[:user][:password],
-  #     password_confirmation: params[:user][:password_confirmation]
-  #   )
-  #
-  #   if @user.save
-  #     session[:user_id] = @user.id
-  #     redirect_to root_path
-  #   else
-  #     flash.now[:alert] = @user.errors.full_messages
-  #     render :new
-  #   end
-  # end
+
+  def create
+    @user = User.new(
+      username: params[:user][:username],
+      first_name: params[:user][:first_name],
+      last_name: params[:user][:last_name],
+      email: params[:user][:email],
+      password: params[:user][:password],
+      password_confirmation: params[:user][:password_confirmation]
+    )
+
+    if @user.save
+      # session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      flash.now[:alert] = @user.errors.full_messages
+      render :new
+    end
+  end
   #
   # def edit
   #   @user = User.find(params[:id])
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  validates :first_name, :last_name, :username, :email, :password, :password_confirmation, presence: true
+
+  has_secure_password
   has_many :reservations
   has_many :restaurants, through: :reservations
 end
