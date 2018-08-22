ce6f1afe1d673305d570d528af380de5b246bf0e
David Ma <davidma415@gmail.com>
2018-08-22 13:15:45 -0400

    Changed landing page

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 resource :user, only: [:new, :create, :edit, :update, :show]
 resources :restaurants do
-  resources :reservations, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  resources :reservations, only: [:index, :new, :create]
 end
+resources :reservations, only: [:show, :edit, :update, :destroy]
 resources :sessions, only: [:new, :create, :destroy]
 
-root 'restaurant#index'
+root 'restaurants#index'
 
 end
