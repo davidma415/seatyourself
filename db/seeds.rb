de3c71d48790c5a117621610f38f06c06081edef
David Ma <davidma415@gmail.com>
2018-08-23 16:47:08 -0400

    Fixed seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,12 +14,8 @@ u1 = User.create!(first_name: "David", last_name: "Ma", username: "DMA", email: 
 u2 = User.create!(first_name: "Joey", last_name: "Saha", username: "JSA", email: "jsa@gmail.com", password: "123", password_confirmation: "123")
 u3 = User.create!(first_name: "Tyler", last_name: "Palef", username: "TPA", email: "tpa@gmail.com", password: "123", password_confirmation: "123")
 
-Restaurant.create(name: "BK", opening_hour: "2000-01-01 08:30:00", closing_hour: "2000-01-01 11:30:00", max_capacity: 45, address: "123 Apple Street", price_range: "$", user_id: 1)
-Restaurant.create(name: "McD", opening_hour: "2000-01-01 08:30:00", closing_hour: "2000-01-01 11:30:00", max_capacity: 35, address: "22 Jump Street", price_range: "$$", user_id: 2)
-Restaurant.create(name: "RL", opening_hour: "2000-01-01 08:30:00", closing_hour: "2000-01-01 11:30:00", max_capacity: 35, user_id: 1)
-
-r1 = Restaurant.create!(name: "BK", opening_hour: "2000-01-01 08:30:00", closing_hour: "2000-01-01 23:30:00", max_capacity: 45, user_id: u1.id, min_party_size: 1, max_party_size: 10)
-r2 = Restaurant.create!(name: "McD", opening_hour: "2000-01-01 08:30:00", closing_hour: "2000-01-01 23:30:00", max_capacity: 35, user_id: u2.id, min_party_size: 2, max_party_size: 20)
+r1 = Restaurant.create!(name: "BK", opening_hour: "2000-01-01 08:30:00", closing_hour: "2000-01-01 23:30:00", address: "123 Apple Street", price_range: "$", max_capacity: 45, user_id: u1.id, min_party_size: 1, max_party_size: 10)
+r2 = Restaurant.create!(name: "McD", opening_hour: "2000-01-01 08:30:00", closing_hour: "2000-01-01 23:30:00", address: "22 Jump Street", price_range: "$$", max_capacity: 35, user_id: u2.id, min_party_size: 2, max_party_size: 20)
 r3 = Restaurant.create!(name: "RL", opening_hour: "2000-01-01 08:30:00", closing_hour: "2000-01-01 23:30:00", max_capacity: 35, user_id: u1.id, min_party_size: 3, max_party_size: 15)
 
 
