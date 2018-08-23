c67cf84290353530eebf8b80209f5cf91ca4e277
David Ma <davidma415@gmail.com>
2018-08-23 10:43:58 -0400

    Added feature to only allow reservations when the restaurant is open

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,6 +2,7 @@ class Reservation < ApplicationRecord
   validates :date, :time, :party_size, presence: true
   validates_numericality_of :party_size, :greater_than => 0
   validate :date_cannot_be_in_the_past
+  validate :restaurant_open
 
   def date_cannot_be_in_the_past
     if date.present? && date.past?
@@ -9,6 +10,11 @@ class Reservation < ApplicationRecord
     end
   end
 
+  def restaurant_open
+    if time < restaurant.opening_hour || time > restaurant.closing_hour
+      errors.add(:time, "is not when the restaurant is open.")
+    end
+  end
   belongs_to :user
   belongs_to :restaurant
 end
