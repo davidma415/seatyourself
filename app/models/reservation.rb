59abbbfd93b66a358039294beb283d2004af373f
David Ma <davidma415@gmail.com>
2018-08-23 10:32:16 -0400

    Added validation for the date of the reservation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,7 +1,14 @@
 class Reservation < ApplicationRecord
   validates :date, :time, :party_size, presence: true
-  validates :party_size, numericality: { only_integer: true }
   validates_numericality_of :party_size, :greater_than => 0
+  validate :date_cannot_be_in_the_past
+
+  def date_cannot_be_in_the_past
+    if date.present? && date.past?
+      errors.add(:date, "can't be in the past")
+    end
+  end
+
   belongs_to :user
   belongs_to :restaurant
 end
