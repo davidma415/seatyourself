2f0b53f59caee413c1af827b0b891a43a1425586
David Ma <davidma415@gmail.com>
2018-08-23 15:33:59 -0400

    Made the username not case sensitive

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :first_name, :last_name, :username, :email, :password, :password_confirmation, presence: true
-  validates :username, uniqueness: true
+  validates :username, :uniqueness => {:case_sensitive => false}
 
   has_secure_password
   has_many :reservations
